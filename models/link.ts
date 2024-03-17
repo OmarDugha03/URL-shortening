ac3df072c59e263adb4adce0c73258427c561d4c
OmarDugha <116832203+OmarDugha03@users.noreply.github.com>
2024-03-17 15:59:47 +0300

    Setup the database , and connect to MongoDB , Also and the models

---
+++ models/link.ts
@@ -0,0 +1,18 @@
+import mongoose, { models, Schema } from "mongoose"
+
+const linkSchema = new Schema(
+  {
+    link: {
+      type: String,
+      require: true,
+      author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  },
+  { timestamps: true }
+)
+
+const Short = models.Short || mongoose.model("Short", linkSchema)
+export default Short
